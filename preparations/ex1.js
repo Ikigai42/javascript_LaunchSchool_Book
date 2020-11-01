a205bc39908f816a32e50178667bd3efe1da325a
Robert Osborn <robdosborn@gmail.com>
2020-11-01 15:01:50 -0600

    Exercise 1 preparations

---
+++ preparations/ex1.js
@@ -0,0 +1,21 @@
+/*
+EXERCISE 1
+//Create directory my_folder
+[~]$ mkdir my_folder
+
+//Navigate to that directory
+[~]$ cd my_folder 
+
+//Create files one.js two.js
+[my_folder]$ touch one.js two.js
+
+//Add code to files
+[my_folder]$ echo "console.log('this is file one');" >> one.js
+[my_folder]$ echo "console.log('this is file two');" >> two.js
+
+//Run programs with node from my_folder directory
+[my_folder]$ node one.js
+=this is file one
+[my_folder]$ node two.js
+=this is file two
+*/
