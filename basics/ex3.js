77e4d830785d8830b1f2c9a23f20ead8f1bc4705
Robert Osborn <robdosborn@gmail.com>
2020-11-03 16:57:52 -0600

    Identify the data type for each value

---
+++ basics/ex3.js
@@ -0,0 +1,19 @@
+/*
+3. Identify the data type for each of the following values:
+
+'true'
+false
+1.5
+2
+undefined
+{ foo: 'bar' }
+
+
+*/
+
+console.log(`'true' is the String data type.`);
+console.log(`false is the Boolean data type.`);
+console.log(`1.5 is the Number data type.`)
+console.log(`2 is the Number data type.`);
+console.log(`undefined is the Undefined data type.`);
+console.log(`{ foo: 'bar' } is the Object data type.`);
