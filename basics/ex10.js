04c37981f1afb2d98dcc9bc2d64bef0ccb86f256
Robert Osborn <robdosborn@gmail.com>
2020-11-03 18:19:03 -0600

    Strict equality operator comparison

---
+++ basics/ex10.js
@@ -0,0 +1,7 @@
+/*
+10. What value does the following expression evaluate to?
+'foo' === 'Foo'
+
+*/
+
+console.log(`The strict equality operator === checks for equality of the two operands, since case matters, 'foo' === 'Foo' returns the Boolean: false.`);
