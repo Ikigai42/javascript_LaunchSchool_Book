6ce4c4a7c0c036089cb8c0f5ee46ffd5f646212f
Robert Osborn <robdosborn@gmail.com>
2020-11-03 18:14:22 -0600

    Create object literal with key-value pairs

---
+++ basics/ex9.js
@@ -0,0 +1,16 @@
+/*
+9. Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+Name	Animal
+asta	dog
+spot	dog
+neptune	fish
+darwin	lizard
+*/
+
+let pets = { 
+  asta: "dog", 
+  spot: "dog", 
+  neptune: "fish", 
+  darwin: "lizard",
+};
