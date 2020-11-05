cb8280a68bbc82177ec4fce45ac636da7d744098
Robert Osborn <robdosborn@gmail.com>
2020-11-05 13:21:24 -0600

    Declare and initialize variable and interpolate calculation into string

---
+++ variables/ex2age.js
@@ -0,0 +1,10 @@
+/*
+2. Write a program named age.js that includes someone's age and then calculates and reports the future age in 10, 20, 30 and 40 years.
+*/ 
+
+let age = 27;
+console.log(`You are ${age} years old.`);
+console.log(`In 10 years, you will be ${10 + age} years old.`);
+console.log(`In 20 years, you will be ${20 + age} years old.`);
+console.log(`In 30 years, you will be ${30 + age} years old.`);
+console.log(`In 40 years, you will be ${40 + age} years old.`);
