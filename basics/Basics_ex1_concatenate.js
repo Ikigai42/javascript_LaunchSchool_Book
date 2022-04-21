cbda835ad789e51bf98b258eda000a25e6877797
Bobby <robdosborn@gmail.com>
2022-04-21 00:26:04 -0500

    Updated Exercise 1 in Basics Chapter

---
+++ basics/Basics_ex1_concatenate.js
@@ -0,0 +1,18 @@
+/*
+Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value.
+For example, if your name is John Doe, think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+
+
+START STATE: input two or more strings with your first name as one input & second input as your last name
+GOAL STATE: concatenate all of the input strings into a single string. 
+CONSTRANTS: are all inputs strings? empty strings?
+
+Which role/s of variable from Sajaniemi’s framework: container to hold string inputs
+*/
+let firstName = 'Ikigai';
+let lastName = 'Code';
+let anotherString = ':)';
+
+let fullName = `${firstName} ${lastName} ${anotherString}`;
+
+console.log(fullName);
