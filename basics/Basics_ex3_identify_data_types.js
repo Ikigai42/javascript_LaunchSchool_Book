a624fd37f4c3f65cfe3af1da2c097a54a30b1535
Ikigai <simplyhappydev@gmail.com>
2022-04-22 04:19:27 -0500

    redoing exercise 3

---
+++ basics/Basics_ex3_identify_data_types.js
@@ -0,0 +1,21 @@
+/*
+3. Identify the data type for each of the following values:
+
+'true'
+false
+1.5
+2
+undefined
+{ foo: 'bar' }
+
+START STATE: various data type inputs
+GOAL STATE: Logging the name of the data type for each input
+CONSTRAINTS: JavaScript Data Types
+*/
+
+console.log(`'true' is a string literal data type value`);
+console.log(`false is a boolean literal data type value`);
+console.log(`1.5 is a numeric literal data type value`);
+console.log(`2 is a numeric literal data type value`);
+console.log(`undefined is the undefined literal data type value`);
+console.log(`{ foo: 'bar' } is an object literal data type value`);
