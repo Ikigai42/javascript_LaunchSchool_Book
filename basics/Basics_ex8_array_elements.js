38e0abda8bf4543933a8007eead1e26ab22b8c84
Ikigai <simplyhappydev@gmail.com>
2022-04-25 03:47:59 -0500

    added additional example and renamed file

---
+++ basics/Basics_ex8_array_elements.js
@@ -0,0 +1,16 @@
+/*
+8. Create an array named names that contains a list of pet names. For instance:
+
+name
+asta
+butterscotch
+neptune
+darwin
+
+*/
+
+let names = ['asta', 'spot', 'neptune', 'darwin'];
+
+let moreNames = ['mr robot', 'somebody', ...names];
+
+console.log(moreNames);
