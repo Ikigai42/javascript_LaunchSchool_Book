7836a76c840affb710a4553ee381cd0cf3b4d25b
Ikigai <simplyhappydev@gmail.com>
2022-04-25 23:24:17 -0500

    renamed file

--- basics/Basics_ex12_string_comparison.js
+++ basics/Basics_ex12_string_comparison.js
@@ -2,11 +2,15 @@
 12. What value does the following expression evaluate to?
 
 '12' < '9'
+*/
 
+console.log(
+  `'12' < '9' Evaluates to the boolean true since '12' is a string and the engine evaluates the first character 1 less than 9`
+);
+
+/*
 book solution
 This question is a bit tricky: the expression evaluates as true since the operands are strings, not numbers. When you compare two strings, JavaScript performs a character-by-character comparison going from left to right, so on the first comparison, it determines that '1' < '9', so '12' must be less than '9'.'
 
 Had you used numbers instead, the expression would evaluate as false.
 */
-
-console.log(`'12' < '9' Evaluates to the boolean true since '12' is a string and the engine evaluates the first character 1 less than 9`);
