247eb17816e888525b7388648abcab762c59426a
Ikigai <simplyhappydev@gmail.com>
2022-04-25 23:14:49 -0500

    renamed file and added notes

--- basics/ex11.js
+++ basics/ex11.js
@@ -7,4 +7,13 @@ It evaluates to 3. parseInt converts strings that begin with a digit to their nu
 
 */
 
-console.log(`parseInt('3.1415') returns the Number 3 since it converts strings to numbers that begin with a digit. It will stop evaluating once it reaches a non-numerical character.`);
+console.log(
+  `parseInt('3.1415') returns the Number 3 since it converts strings to numbers that begin with a digit. It will stop evaluating once it reaches a non-numerical character.`
+);
+console.log(`Syntax: parseInt(string, radix)`);
+console.log(`-If the first character cannot be converted, NaN is returned.
+-Leading and trailing spaces are ignored.
+-A radix parameter specifies the number system to use:
+-2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal.
+-If radix is omitted, JavaScript assumes radix 10. 
+-If the value begins with "0x", JavaScript assumes radix 16.`);
