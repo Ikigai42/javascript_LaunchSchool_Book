0cfb127e76bae4ee1cc9c23a243104b9f944c48e
Ikigai <simplyhappydev@gmail.com>
2022-04-25 23:04:34 -0500

    renamed file & added string ASCII codes

---
+++ basics/Basics_ex10_strict_equality_operator.js
@@ -0,0 +1,12 @@
+/*
+10. What value does the following expression evaluate to?
+'foo' === 'Foo'
+
+*/
+
+console.log(
+  `The strict equality operator === checks for equality of the two operands, since case matters, 'foo' === 'Foo' returns the Boolean: false.`
+);
+console.log(
+  `The string foo converted to ASCII is 102 111 111 and Foo converted to ASCII is 70 111 111.`
+);
--- basics/ex10.js
+++
@@ -1,7 +0,0 @@
-/*
-10. What value does the following expression evaluate to?
-'foo' === 'Foo'
-
-*/
-
-console.log(`The strict equality operator === checks for equality of the two operands, since case matters, 'foo' === 'Foo' returns the Boolean: false.`);
