ddcc4a166564dbff832227b743e4e56ab627e9aa
Bobby <simplyhappydev@gmail.com>
2022-04-26 23:51:43 -0500

    renamed file

--- variables/Variables_ex4_syntax_error.js
+++ variables/Variables_ex4_syntax_error.js
@@ -10,7 +10,7 @@ NAME = 'Joe';
 console.log('Good Morning, ' + NAME);
 console.log('Good Afternoon, ' + NAME);
 console.log('Good Evening, ' + NAME);
-*/ 
+*/
 
 console.log(`What happens when you run the following code? Why?
 const NAME = 'Victor';
