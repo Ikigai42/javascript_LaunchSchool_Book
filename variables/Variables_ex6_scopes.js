8afbc8ce44edd457cf7d311db6f4eaa7a3f2bf7b
Bobby <simplyhappydev@gmail.com>
2022-04-28 01:22:54 -0500

    renamed file

--- variables/Variables_ex6_scopes.js
+++ variables/Variables_ex6_scopes.js
@@ -7,9 +7,6 @@ const FOO = 'bar';
 }
 
 console.log(FOO);
-
-Book
- this program doesn't raise an error, and it logs bar on line 6. One key difference, though, is that we are using const instead of let, which may have led you to believe an error would occur on line 3. However, since the two const variables are separate entities, no error occurs.
 */
 console.log(`Will this program produce an error when run? Why or why not?
 const FOO = 'bar';
@@ -20,10 +17,15 @@ const FOO = 'bar';
 console.log(FOO);
 
 No error will be produced because the constant FOO is declared in different scopes & logs the String value "bar". 
-`)
+`);
 const FOO = 'bar';
 {
   const FOO = 'qux';
 }
 
 console.log(FOO);
+
+/**
+ Book
+ this program doesn't raise an error, and it logs bar on line 6. One key difference, though, is that we are using const instead of let, which may have led you to believe an error would occur on line 3. However, since the two const variables are separate entities, no error occurs.
+ */
