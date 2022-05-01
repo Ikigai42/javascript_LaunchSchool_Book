865406fa7bf7cb19b09e727edb036d68999f8df4
Bobby <simplyhappydev@gmail.com>
2022-04-30 23:18:06 -0500

    renamed files and added chapter examples

--- variables/age.js
+++ variables/age.js
@@ -1,6 +1,6 @@
 /*
 2. Write a program named age.js that includes someone's age and then calculates and reports the future age in 10, 20, 30 and 40 years.
-*/ 
+*/
 
 let age = 27;
 console.log(`You are ${age} years old.`);
