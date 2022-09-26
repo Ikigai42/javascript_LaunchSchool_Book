dfdb6f3f179aa284d1ce85d660536974fa940411
Bobby <simplyhappydev@gmail.com>
2022-09-26 18:57:16 -0500

    Completing preparations-exercise 1 again

--- 0-preparations/ex1.js
+++ 0-preparations/ex1.js
@@ -7,20 +7,20 @@ Add some code to two.js that logs 'this is file two'.
 When you're finished, run both programs with node.
 ***
 
-//Create directory my_folder
+Create directory my_folder
 [~]$ mkdir my_folder
 
-//Navigate to that directory
+Navigate to that directory
 [~]$ cd my_folder 
 
-//Create files one.js two.js
+Create files one.js two.js
 [my_folder]$ touch one.js two.js
 
-//Add code to files
+Add code to files
 [my_folder]$ echo "console.log('this is file one');" >> one.js
 [my_folder]$ echo "console.log('this is file two');" >> two.js
 
-//Run programs with node from my_folder directory
+Run programs with node from my_folder directory
 [my_folder]$ node one.js
 =this is file one
 [my_folder]$ node two.js
