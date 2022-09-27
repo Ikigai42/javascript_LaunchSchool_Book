b4c0d693aa6e54091d50e38d31ad9ed9732fbea2
Bobby <simplyhappydev@gmail.com>
2022-09-26 19:36:15 -0500

    Exercise 4: running file in node REPL & Chrome Console

---
+++ 0-preparations_exercises/preparations_ex4_node-REPL.js
@@ -0,0 +1,30 @@
+/*
+4. 1. Use node to run the foo.js file using node. What does it output?
+
+[0-preparations_exercises (main)]$ node preparations_ex3_foo.js
+bar
+
+   2. Copy and paste the code from foo.js into the node REPL. What does it output?
+
+[0-preparations_exercises (main)]$ node
+Welcome to Node.js v14.17.0.
+Type ".help" for more information.
+> let foo = 'bar';
+undefined
+> console.log(foo);
+bar
+undefined
+> foo;
+'bar'
+
+   3. Copy and paste the code from foo.js into the Chrome console REPL. What does it output?
+
+> let foo = 'bar';
+< undefined
+> console.log(foo);
+  bar
+< undefined  
+> foo;
+< 'bar'
+
+*/
