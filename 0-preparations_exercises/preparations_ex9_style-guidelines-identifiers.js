059e6ed0fd23060985a9ba45b32838307e248223
Bobby <simplyhappydev@gmail.com>
2022-09-27 15:00:59 -0500

    Exercise 9 eliminate invalid identifier names

---
+++ 0-preparations_exercises/preparations_ex9_style-guidelines-identifiers.js
@@ -0,0 +1,15 @@
+/*
+9. Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+not Valid indentifier name?
+
+Name
+*/
+index // is Valid 
+CatName // is Valid
+snake_case // not Valid > snake_case
+lazyDog // is Valid
+quick_Fox // not Valid > snake_case
+1stCharacter // not Valid > first character is a number
+operand2 // is Valid
+BIG_NUMBER // is Valid
