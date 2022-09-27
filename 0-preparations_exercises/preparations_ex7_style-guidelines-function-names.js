eda4def35ae47382a5793d9b86ce4bff7e57eb95
Bobby <simplyhappydev@gmail.com>
2022-09-27 14:44:31 -0500

    Exercise 7 JS style guidelins for function names

---
+++ 0-preparations_exercises/preparations_ex7_style-guidelines-function-names.js
@@ -0,0 +1,13 @@
+/**
+7. Which of the following names satisfy the style guidelines for function names?
+
+Valid function Name?
+ */
+index // is Valid > uses all aceptable characters and in correct case
+CatName // is Valid > constructor functions use PascalCase
+snake_case // not Valid > snake_case
+lazyDog // is Valid > uses camelCase
+quick_Fox // not Valid > snake_case
+1stCharacter // not Valid > first character cannot be a number
+operand2 // is Valid > contains number but not 1st character
+BIG_NUMBER // not Valid > uses SCREAMING_SNAKE_CASE
