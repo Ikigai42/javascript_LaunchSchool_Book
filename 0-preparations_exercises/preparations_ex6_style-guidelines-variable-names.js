d85b35bb1f4dbb84fd71a6d4b05dbcce36bec9d7
Bobby <simplyhappydev@gmail.com>
2022-09-27 13:58:24 -0500

    Exercise 6 JS style guidelines for variable names

---
+++ 0-preparations_exercises/preparations_ex6_style-guidelines-variable-names.js
@@ -0,0 +1,16 @@
+/*
+6. Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables.
+
+Are variable names valid?
+
+Name:
+*/
+index // is Valid
+CatName // not Valid > pascal case that caps first word is only for constructors > camel case is used for variable names
+snake_case // not Valid > uses snake_case instead of camel case
+lazyDog // is Valid
+quick_Fox // not Valid > uses snake_case instead of camel case
+1stCharacter // not Valid > cannot start variable declaration with numeric
+operand2 // is Valid
+BIG_NUMBER // not Valid > Screaming snake case is for constants and magic numbers
+
