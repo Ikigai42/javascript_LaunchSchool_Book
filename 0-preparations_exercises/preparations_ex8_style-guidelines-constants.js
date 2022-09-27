66de8ac69cd3d1267d8563a2e7b119df6b6ca49a
Bobby <simplyhappydev@gmail.com>
2022-09-27 14:52:41 -0500

    Exercise 8 JS style guidelines for constants

---
+++ 0-preparations_exercises/preparations_ex8_style-guidelines-constants.js
@@ -0,0 +1,15 @@
+/*
+8. Which of the following names satisfy the style guidelines for constants used to represent magic numbers?
+
+Is valid constant name?
+
+Name:
+*/
+index // not Valid > lowercase
+CatName // not Valid > PascalCase
+snake_case // not Valid > snake_case
+lazyDog // not Valid > lowercase
+quick_Fox // not Valid > snake_case
+1stCharacter // not Valid > number is first character and other characters not all correct case
+operand2 // not Valid > characters not valid case
+BIG_NUMBER // Valid > all characters correct case UPPER_CASE called SCREAMING_SNAKE_CASE 
