1a97e5514d0717a46f6fd309f42dfebf4c2c7229
Bobby <simplyhappydev@gmail.com>
2022-09-28 16:24:35 -0500

    Using variables concatenate string values

---
+++ 1-the_basics/ex1_concatenate_strings.js
@@ -0,0 +1,12 @@
+/*
+1. Concatenate two or more strings, one with your first name and one with your last, to create a string with your full name as its value. For example, if your name is John Doe, think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+*/
+
+//Initialize the firstName variable and assign it the string value "Maya"
+let firstName = 'Maya';
+//Initialize the lastName variable and assign it the string value "Angelou"
+let lastName = 'Angelou';
+//Initialize the fullName variable and assign it the current value of firstName concatenated an empty string and the current value of the lastName variable
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
