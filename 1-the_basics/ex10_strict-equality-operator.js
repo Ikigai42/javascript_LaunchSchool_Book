6a710b1b48ae4152660167337b470eace27665b8
Bobby <simplyhappydev@gmail.com>
2022-09-29 18:16:15 -0500

    Ex 10 strict equality operator on strings with different cases

---
+++ 1-the_basics/ex10_strict-equality-operator.js
@@ -0,0 +1,8 @@
+/*
+10. What value does the following expression evaluate to?
+'foo' === 'Foo'
+*/
+
+//Case matters when comparing strings so, the expression evaluates to the boolean value false.
+
+console.log('foo' === 'Foo');
