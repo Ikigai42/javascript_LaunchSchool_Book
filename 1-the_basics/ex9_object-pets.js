f92abb845cf48663adccd20c2d3314627391d247
Bobby <simplyhappydev@gmail.com>
2022-09-29 18:03:18 -0500

    Ex9 adding key/value pairs to a pets object

---
+++ 1-the_basics/ex9_object-pets.js
@@ -0,0 +1,20 @@
+/*
+9. Create an object named pets that contains a list of pet names and the type of animal. For instance:
+
+Name	Animal
+asta	dog
+butterscotch	cat
+pudding	cat
+neptune	fish
+darwin	lizard
+
+You can use that data or make up your own.
+*/
+let pets = {
+  astra: 'dog',
+  canela: 'dog',
+  ronin: 'cat',
+  arete: 'horse', //optional comma on last line
+};
+
+console.log(pets);
