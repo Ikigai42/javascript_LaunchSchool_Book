13827a004ed55898c60ecea01c609130b98f958e
Bobby <simplyhappydev@gmail.com>
2022-10-02 15:37:06 -0500

    Added LS solution comments

--- 4-functions/ex1-function-scope.js
+++ 4-functions/ex1-function-scope.js
@@ -10,3 +10,7 @@ function foo() {
 
 foo(); //function invocation of foo() which has no explicit return value so returns default: undefined
 console.log(bar); //function call logging the current value of the variable bar, 1, of the global scope
+
+/*LS
+The code logs 1 to the console. foo doesn't affect the value assigned to bar on line 1 since JavaScript functions create an inner scope. Thus, the bar variable on line 3 is not the same as the one on line 1. In the end, foo() has no bearing on the final output.
+*/
