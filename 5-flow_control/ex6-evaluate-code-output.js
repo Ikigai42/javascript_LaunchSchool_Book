abc68332ca9b5a9c8d0f1068faaab9bc35581183
Bobby <simplyhappydev@gmail.com>
2022-10-03 17:02:38 -0500

    Ex6 empty array is truthy

---
+++ 5-flow_control/ex6-evaluate-code-output.js
@@ -0,0 +1,16 @@
+//6. What does this code output to the console?
+
+function isArrayEmpty(arr) {
+  if (arr) {
+    console.log('Not Empty');
+  } else {
+    console.log('Empty');
+  }
+}
+
+isArrayEmpty([]); //passing an empty array literal intro the isArrayEmpty() function as an argument
+
+//logs 'Not Empty' since [] evaluates to a truthy value
+
+/*LS
+The output is Not Empty since, while the array is empty -- it has no elements and the length property is 0 -- it isn't falsy. Thus, JavaScript executes the first branch of the if statement. */
