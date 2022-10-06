c2545551d9e404dddf2929a7876351da6df7c8b9
Bobby <simplyhappydev@gmail.com>
2022-10-06 15:30:12 -0500

    Ex1 using dot and bracket notation to access object's value

---
+++ 8-objects/ex1-object-value.js
@@ -0,0 +1,13 @@
+//1. Given the following code, how can you access the name of the person?
+
+let person = {
+  name: 'Bob',
+  occupation: 'web developer',
+  hobbies: 'painting',
+};
+
+//Acces name value of the person object literal
+//dot notation
+console.log(person.name);
+//bracket notation
+console.log(person['name']);
