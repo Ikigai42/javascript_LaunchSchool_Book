50efe47683c6bb21b72eac9493090d74b6748852
Bobby <simplyhappydev@gmail.com>
2022-10-05 19:04:16 -0500

    Ex10 chaining brackets to reassign value in subarray

---
+++ 7-arrays/ex10-nested-array-value.js
@@ -0,0 +1,13 @@
+//10.
+
+let arr = [
+  ['hello', 'world'],
+  ['example', 'mem', null, 6, 88],
+  [4, 8, 12],
+];
+console.log(arr);
+console.log(`Ressign: ${arr[1][3]} to 606`);
+
+arr[1][3] = 606; //reassignment to number value 606
+
+console.log(arr);
