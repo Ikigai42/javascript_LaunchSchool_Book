8df7662e2c6aa8e536fc95831a3b2d8fb790f532
Bobby <simplyhappydev@gmail.com>
2022-10-08 14:35:31 -0500

    Ex4 using the Math.max() method & array destructering to find array's max values

---
+++ 9-more_stuff/ex4.list-max-val.js
@@ -0,0 +1,14 @@
+let arr1 = [1, 6, 3, 2];
+let maxValArr1 = Math.max(...arr1);
+console.log(`let arr1 = [1, 6, 3, 2];`);
+console.log(`Max value from arr1 array: ${maxValArr1}`);
+
+let arr2 = [-1, -6, -3, -2];
+let maxValArr2 = Math.max(...arr2);
+console.log(`let arr2 = [-1, -6, -3, -2];`);
+console.log(`Max value from arr2 array: ${maxValArr2}`);
+
+let arr3 = [2, 2];
+let maxValArr3 = Math.max(...arr3);
+console.log(`let arr3 = [2, 2];`);
+console.log(`Max value from arr3 array: ${maxValArr3}`);
